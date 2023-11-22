423d40fb128442dce8c2df839f8e4b7f5e6d2e14
Leonardo <LeoCA2176@outlook.com>
2023-11-22 11:30:39 -0600

    Primer versión

---
+++ SPC23.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int respuesta=0;
+    
+        cout<<"----------Soccer Player Career 2023 ----------"<<endl<<endl;
+        cout<<"Bienvenido a SPC23"<<endl<<endl;
+        cout<<"Seleccione una opión para continuar"<<endl<<endl;
+        cout<<"1.Salir"<<endl;
+        cout<<"2.Jugar"<<endl;
+        cout<<"3.Controles"<<endl;
+        cout<<"4.Sobre el juego"<<endl;
+        cin>>respuesta; 
+return 0;
+}
